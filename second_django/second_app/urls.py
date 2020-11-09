252a360141d5b5abc33d42398b9fe9527ab5cdec
Mane <maneyeganyan@yahoo.com>
2020-11-09 22:04:29 +0400

    hyperlinks

--- second_django/second_app/urls.py
+++ second_django/second_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import greeting, introduction, date, square
+from .views import main_page, greeting, introduction, date, square
 
 
 urlpatterns = [
+	path('', main_page, name = "main_page"),
     path('greeting/', greeting, name = "greeting"),    
     path('introduction/', introduction, name = "introduction"),
     path('date/', date, name = "date"),
--- second_django/second_app/views.py
+++ second_django/second_app/views.py
@@ -4,21 +4,32 @@ from datetime import datetime
 
 # Create your views here.
 
+def main_page(request):
+	return HttpResponse("<h1> We have 4 links to show you some pages of our little project, lets try them! </h1> \
+		<h2> <a href=/greeting>greeting</a> \
+		<a href=/introduction>introduction</a> \
+		<a href=/date>date</a> \
+		<a href=/square>square</a> </h2>")
+
 def greeting(request):
-	return HttpResponse("Hallo, welcome to my second DJango project. I promise, that you won't \
-		boring here")
+	return HttpResponse("<h1> Hallo, welcome to my second DJango project. I promise, that you won't \
+		get bored here </h1> \
+		<a href=http://127.0.0.1:8000>back</a>")
 
 def introduction(request):
-	return HttpResponse("This is a page for our lessons. You can find here today's Date if you want. Just \
-		write 'date' in url and you can see it")
+	return HttpResponse("<h1>This is a page for our lessons. You can find here today's Date if you want. Just \
+		click on the 'date' button and you can see it</h1> \
+		<a href=http://127.0.0.1:8000>back</a>")
 
 def date(request):
 	this_date = datetime.now()
-	return HttpResponse(f"Today's Date is {this_date}")
+	return HttpResponse(f"<h1>Today's Date is {this_date}</h1>\
+		<a href=http://127.0.0.1:8000>back</a>")
 
 def square(request):
 	dict_1 = {}
 	for i in range(1,16):
 		dict_1[i] = i**2
 
-	return HttpResponse(f'{dict_1}')
+	return HttpResponse(f'<h1>{dict_1}</h1>\
+		<a href=http://127.0.0.1:8000>back</a>')
